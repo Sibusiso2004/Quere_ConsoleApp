46a482f4fc2ccb220e62cda2a24b4b2ea78260d2
Haides Hagarama <sibusisobheka198@gmail.com>
2025-04-24 09:36:32 +0200

    This is the Quere Console app

---
+++ Customer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quere
+{
+    public class Customer
+    {
+        //personal Details
+        public string Fullname { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+
+        //Order Details
+        public string Category { get; set; }
+        public string KitType { get; set; }
+        public string Size { get; set; }
+
+        public DateTime OrderDate = DateTime.Now;
+        public int Quantity { get; set; }
+        public double TotalPrice { get; set; }
+        public double TotalDiscount { get; set; }
+        public double FinalTotalOrderAmount { get; set; }
+
+        //sortout
+       
+
+
+        //methods
+        public double CalcBasic()
+        {
+            double Basicprice = 7699.95;
+
+            TotalPrice = Basicprice * Quantity;
+            return TotalPrice;
+           
+        }
+
+
+        public double CustomerCategoryDiscount()
+        {
+            double CustomerDiscount = 0;
+            if (Category == "Platinum")
+            {
+                CustomerDiscount = CalcBasic() * 0.05;
+            }
+            else if (Category == "Gold")
+            {
+                CustomerDiscount = CalcBasic() * 0.10;
+            }
+            else if (Category == "Silver")
+            {
+                CustomerDiscount = CalcBasic() * 0.05;
+            }
+
+            return CustomerDiscount;
+        }
+
+        public double CalcChristmasDiscount()
+        {
+            double ChristmasDiscount = 0;
+            if (Gender == "Female")
+            {
+                ChristmasDiscount = CalcBasic() * 0.08;
+            }
+            else
+            {
+                ChristmasDiscount = 0;
+            }
+
+            return ChristmasDiscount;
+
+        }
+
+        public double TotalDiscountGiven()
+        {
+            TotalDiscount = CustomerCategoryDiscount() + CalcChristmasDiscount();
+            return TotalDiscount;
+        }
+
+        public double FinalTotalAmount()
+        {
+            FinalTotalOrderAmount = CalcBasic() - TotalDiscountGiven();
+            return FinalTotalOrderAmount;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Quere
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer obj = new Customer();
+
+           
+            //this is a long reminder. its been 3 years since i coded a console application, so computer.... please bare with me on this one.
+            Console.Write("Enter your name please: ");
+            obj.Fullname = Console.ReadLine();
+
+            Console.Write("Enter your surname please: ");
+            obj.Surname = Console.ReadLine();
+
+            Console.Write("Enter your email please: ");
+            obj.Email = Console.ReadLine();
+
+            Console.Write("Enter your Gender { Male/Female }: ");
+            obj.Gender = Console.ReadLine();
+
+            Console.Write("Enter your Catergory { Platinum/Gold/Silver }: ");
+            obj.Category = Console.ReadLine();
+
+            Console.Write("Enter your Kit type { Classic/Modern }: ");
+            obj.KitType = Console.ReadLine();
+
+            Console.Write("Enter your Kit size { Small/Medium/Large }: ");
+            obj.Size = Console.ReadLine();
+
+            Console.Write("Enter the quantity: ");
+            obj.Quantity =int.Parse(Console.ReadLine());
+
+            //run Methods
+            obj.CalcBasic();
+            obj.CustomerCategoryDiscount();
+            obj.CalcChristmasDiscount();
+            obj.TotalDiscountGiven();
+            obj.FinalTotalAmount();
+
+            //print
+            Console.WriteLine("");
+            Console.WriteLine(".............Quote.............");
+            Console.WriteLine("");
+            Console.WriteLine("Full Name: "+ obj.Fullname.ToString());
+            Console.WriteLine("Surname: " + obj.Surname.ToString());
+            Console.WriteLine("Gender: " + obj.Gender.ToString());
+            Console.WriteLine("Category: " + obj.Category.ToString());
+            Console.WriteLine("KitType: " + obj.KitType.ToString());
+            Console.WriteLine("Size: " + obj.Size.ToString());
+            Console.WriteLine("Order Date: " + obj.OrderDate.ToString("G"));
+            Console.WriteLine("Total Price: R " + obj.CalcBasic());
+            Console.WriteLine("Total Discount Amount: R " + obj.TotalDiscountGiven());
+            Console.WriteLine("Final Amount: R " + obj.FinalTotalAmount());
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
